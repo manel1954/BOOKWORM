dd380175b3ff66988e2746f7c74fd6ec8dac9de4
Manel <ea3eiz@ea3eiz.com>
2023-12-15 01:06:28 +0100

    Update ejecutar_radio.sh

--- ejecutar_radio.sh
+++ ejecutar_radio.sh
@@ -38,7 +38,7 @@ sleep 1
 sudo rm /home/pi/Abrir_Radio.desktop
 
 cd /home/pi/MMDVMHost
-xterm -geometry 88x16+2+665 -bg blue -fg white -fa ‘verdana’ -fs 9x -T CONSOLA_RADIO -e sudo ./MMDVMHost MMDVM.ini
+xterm -geometry 88x16+1000+665 -bg blue -fg white -fa ‘verdana’ -fs 9x -T CONSOLA_RADIO -e sudo ./MMDVMHost MMDVM.ini
 
 cd /home/pi/Desktop
 sudo cp Abrir_Radio.desktop /home/pi
