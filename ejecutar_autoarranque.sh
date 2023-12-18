e80d04ca9561ea26d1f604ec5c4d1da7a2859eea
Manel <ea3eiz@ea3eiz.com>
2023-12-18 10:29:15 +0100

    1

--- ejecutar_autoarranque.sh
+++ ejecutar_autoarranque.sh
@@ -3,8 +3,8 @@ SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_autoarranque.desktop /home/pi
 sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh cerrar_autoarranque.sh'" /home/pi/Abrir_autoarranque.desktop
-sed -i "7c Icon=/home/pi/$SCRIPTS_version/SOLO_YSF_ON.png" /home/pi/Abrir_autoarranque.desktop
-sed -i "11c Name[es_ES]=Cerrar solo FUSION" /home/pi/Abrir_solofusion.desktop
+sed -i "7c Icon=/home/pi/$SCRIPTS_version/ICO_AUTOARRANQUE.png" /home/pi/Abrir_autoarranque.desktop
+sed -i "11c Name[es_ES]=Cerrar AUTOARRANQUE" /home/pi/Abrir_autoarranque.desktop
 sed -i "12c SOLOFUSION=ON" /home/pi/status.ini
 cd /home/pi
 sudo cp Abrir_autoarranque.desktop /home/pi/Desktop
