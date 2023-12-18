cf3da2b5e5be40083a93162da21259a26fb21fbb
Manel <ea3eiz@ea3eiz.com>
2023-12-18 09:55:26 +0100

    Update actualizar_YSF.sh

--- actualizar_YSF.sh
+++ actualizar_YSF.sh
@@ -8,23 +8,24 @@ AMARILLO="\033[1;33m"
 CIAN="\033[1;36m"
 GRIS="\033[0m"
 MARRON="\33[38;5;138m"
+NEGRO="\33[0;30m"
 
 # path usuario
 usuario="/home/pi"
 usuario="$usuario"
 						
 						echo "\v\v\v\v\v"
-						echo "${VERDE}"
+						echo "${NEGRO}"
 						echo "   **************************************************************************"
 						echo "   **************************************************************************"
-						echo "${VERDE}"
+						echo "${NEGRO}"
 						echo "                  XXXXXXXX ACTUALIZANDO LISTADO DE SALAS YSF y FCS                   "
 						echo "                                                                             "
-						echo -n "${VERDE}"
+						echo -n "${NEGRO}"
 						echo "   **************************************************************************"
 						echo "   **************************************************************************"
 						sleep 3
-			            echo "${VERDE}"						
+			            echo "${NEGRO}"						
 						cd $usuario/YSFClients/YSFGateway
 						sudo chmod 777 -R $usuario/YSFClients/YSFGateway
 						sudo wget -O YSFHosts.txt http://register.ysfreflector.de/export_csv.php					
@@ -32,13 +33,13 @@ usuario="$usuario"
 						sleep 3
 						clear
 						echo "\v\v\v\v\v"
-						echo "${BLANCO}"
+						echo "${NEGRO}"
 						echo "   **************************************************************************"
 						echo "   **************************************************************************"
-						echo "${VERDE}"
+						echo "${NEGRO}"
 						echo "             Los fichero YSFHost.txt y FCSRooms.txt se han actualizado xxx      "
 						echo "                                                                             "
-						echo -n "${BLANCO}"
+						echo -n "${NEGRO}"
 						echo "   **************************************************************************"
 						echo "   **************************************************************************"
 						sleep 3
