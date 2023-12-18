fe89816cbfd08f053655f98da7398ee6a6358be9
Manel <ea3eiz@ea3eiz.com>
2023-12-18 12:05:58 +0100

    Update ejecutar_autoarranque.sh

--- ejecutar_autoarranque.sh
+++ ejecutar_autoarranque.sh
@@ -3,7 +3,7 @@ SCRIPTS_version=$(awk "NR==3" /home/pi/version-fecha-actualizacion)
 cd /home/pi/Desktop
 sudo cp Abrir_autoarranques.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh cerrar_autoarranque.sh'" /home/pi/Abrir_autoarranques.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_AUTOARRANQUE.png" /home/pi/Abrir_autoarranques.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_DMRPLUS_ON.png.png" /home/pi/Abrir_autoarranques.desktop
 sed -i "10c Name[es_ES]=Cerrar AUTOARRANQUE" /home/pi/Abrir_autoarranques.desktop
 #sed -i "12c SOLOFUSION=ON" /home/pi/status.ini
 cd /home/pi
