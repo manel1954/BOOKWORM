437ee46a41aa1b48a1a8784abdaa890acc70aabe
Manel <ea3eiz@ea3eiz.com>
2023-12-18 11:11:15 +0100

    1

--- cerrar_autoarranque.sh
+++ cerrar_autoarranque.sh
@@ -2,17 +2,13 @@
 SCRIPTS_version=$(awk "NR==3" /home/pi/version-fecha-actualizacion)
 cd /home/pi/Desktop
 sudo cp Abrir_autoarranques.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh cerrar_autoarranque.sh'" /home/pi/Abrir_autoarranques.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh ejecutar_autoarranque.sh'" /home/pi/Abrir_autoarranques.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_AUTOARRANQUE.png" /home/pi/Abrir_autoarranques.desktop
-sed -i "10c Name[es_ES]=Cerrar AUTOARRANQUE" /home/pi/Abrir_autoarranques.desktop
+sed -i "10c Name[es_ES]=Abrir AUTOARRANQUE" /home/pi/Abrir_autoarranques.desktop
 #sed -i "12c SOLOFUSION=ON" /home/pi/status.ini
 cd /home/pi
 sudo cp Abrir_autoarranques.desktop /home/pi/Desktop
 sudo rm /home/pi/Abrir_autoarranques.desktop
 
 cd /home/pi/BOOKWORM/qt
- ./qt_autoarranque
-
-
-
-
+ ./qt_autoarranque_cerrar
