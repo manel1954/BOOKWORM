2cb0edd3e53ade5b31ac94927e6ab93e74c23810
Manel <ea3eiz@ea3eiz.com>
2023-12-18 10:41:02 +0100

    Update ejecutar_autoarranque.sh

--- ejecutar_autoarranque.sh
+++ ejecutar_autoarranque.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
-sudo cp Abrir_autoarranque.desktop /home/pi
-sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh cerrar_autoarranque.sh'" /home/pi/Abrir_autoarranque.desktop
-sed -i "7c Icon=/home/pi/$SCRIPTS_version/ICO_AUTOARRANQUE.png" /home/pi/Abrir_autoarranque.desktop
-sed -i "11c Name[es_ES]=Cerrar AUTOARRANQUE" /home/pi/Abrir_autoarranque.desktop
+sudo cp Abrir_autoarranques.desktop /home/pi
+sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh cerrar_autoarranque.sh'" /home/pi/Abrir_autoarranques.desktop
+sed -i "7c Icon=/home/pi/$SCRIPTS_version/ICO_AUTOARRANQUE.png" /home/pi/Abrir_autoarranques.desktop
+sed -i "11c Name[es_ES]=Cerrar AUTOARRANQUE" /home/pi/Abrir_autoarranques.desktop
 sed -i "12c SOLOFUSION=ON" /home/pi/status.ini
 cd /home/pi
-sudo cp Abrir_autoarranque.desktop /home/pi/Desktop
-sudo rm /home/pi/Abrir_autoarranque.desktop
+sudo cp Abrir_autoarranques.desktop /home/pi/Desktop
+sudo rm /home/pi/Abrir_autoarranques.desktop
 
 cd /home/pi/BOOKWORM/qt
  ./qt_autoarranque
