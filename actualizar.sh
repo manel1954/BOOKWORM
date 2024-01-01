9f2a5db4735ef962b4a30f8daa926e2528c48d29
Manel <ea3eiz@ea3eiz.com>
2024-01-02 00:15:41 +0100

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -28,7 +28,7 @@ usuario="/home/pi"
 usuario="$usuario"
 SCRIPTS_version="BOOKWORM"
 actualizacion=$(awk "NR==2" /home/pi/version-fecha-actualizacion)
-version="SYST_"
+version="PI5_"
 version=$version$actualizacion
 
 #pone todos los status de inicio en OFF
