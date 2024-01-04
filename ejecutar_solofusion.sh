98fbb79c27c61803a97ee10fc6a9c9cb4948469f
Manel <ea3eiz@ea3eiz.com>
2024-01-04 19:05:00 +0100

    Update ejecutar_solofusion.sh

--- ejecutar_solofusion.sh
+++ ejecutar_solofusion.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#sudo killall -9 qt_editor_solofusion
+sudo killall -9 qt_editor_solofusion
 
 mode=`grep -n -m 1 "^UARTPort=" /home/pi/MMDVMHost/MMDVMFUSION.ini`
 buscar=":"
@@ -26,15 +26,15 @@ sed -i "6c $frecuencia" /home/pi/INFO_RXF
 
 SCRIPTS_version=$(awk "NR==3" /home/pi/version-fecha-actualizacion)
 cd /home/pi/Desktop
-cp Abrir_solofusion.desktop /home/pi
-sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sh cerrar_solofusion.sh'" /home/pi/Abrir_solofusion.desktop
-sed -i "7c Icon=/home/pi/$SCRIPTS_version/ICO_SOLOFUSION_ON.png" /home/pi/Abrir_solofusion.desktop
-sed -i "11c Name[es_ES]=Cerrar solo FUSION" /home/pi/Abrir_solofusion.desktop
-sed -i "12c SOLOFUSION=ON" /home/pi/status.ini
+sudo cp Abrir_solofusion.desktop /home/pi
+sudo sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sh cerrar_solofusion.sh'" /home/pi/Abrir_solofusion.desktop
+sudo sed -i "7c Icon=/home/pi/$SCRIPTS_version/ICO_SOLOFUSION_ON.png" /home/pi/Abrir_solofusion.desktop
+sudo sed -i "11c Name[es_ES]=Cerrar solo FUSION" /home/pi/Abrir_solofusion.desktop
+sudo sed -i "12c SOLOFUSION=ON" /home/pi/status.ini
 cd /home/pi
-cp Abrir_solofusion.desktop /home/pi/Desktop
+sudo cp Abrir_solofusion.desktop /home/pi/Desktop
 
-rm /home/pi/Abrir_solofusion.desktop
+sudo rm /home/pi/Abrir_solofusion.desktop
 
 cd /home/pi/YSFClients/YSFGateway
 xterm -geometry 85x12+1215+296 -bg black -fg orange -fa ‘verdana’ -fs 9x -T CONSOLA_YSFGateway -e ./YSFGateway YSFGateway.ini & 
