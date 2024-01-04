d3581e1c6aaa6d756073f27317a4ae11033938fd
Manel <ea3eiz@ea3eiz.com>
2024-01-05 00:16:38 +0100

    1

--- cerrar_solofusion.sh
+++ cerrar_solofusion.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 sudo killall MMDVMFUSION
 sudo killall YSFGateway
-SCRIPTS_version=$(awk "NR==3" /home/pi/version-fecha-actualizacion)
-cd /home/pi/Desktop
-sudo cp Abrir_solofusion.desktop /home/pi
 
-sudo sed -i "6c Exec=sh -c 'cd /home/pi/BOOKWORM/;sh ejecutar_solofusion.sh'" /home/pi/Abrir_solofusion.desktop
-sudo sed -i "7c Icon=/home/pi/BOOKWORM/ICO_SOLOFUSION_OFF.png" /home/pi/Abrir_solofusion.desktop
-sudo sed -i "11c Name[es_ES]=Abrir solo FUSION" /home/pi/Abrir_solofusion.desktop
-sudo sed -i "12c SOLOFUSION=OFF" /home/pi/status.ini
 
-cd /home/pi
-sudo cp Abrir_solofusion.desktop /home/pi/Desktop
 
-sudo rm /home/pi/Abrir_solofusion.desktop
+sed -i "6c Exec=sh -c 'cd /home/pi/BOOKWORM/;sh ejecutar_solofusion.sh'" /home/pi/Abrir_solofusion.desktop
+sed -i "7c Icon=/home/pi/BOOKWORM/ICO_SOLOFUSION_OFF.png" /home/pi/Abrir_solofusion.desktop
+sed -i "11c Name[es_ES]=Abrir solo FUSION" /home/pi/Abrir_solofusion.desktop
+sed -i "12c SOLOFUSION=OFF" /home/pi/status.ini
+
+
+
+
 
 
--- ejecutar_solofusion.sh
+++ ejecutar_solofusion.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-SCRIPTS_version=$(awk "NR==3" /home/pi/version-fecha-actualizacion)
-cd /home/pi/Desktop
-sudo cp Abrir_solofusion.desktop /home/pi
-sudo sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sh cerrar_solofusion.sh'" /home/pi/Abrir_solofusion.desktop
-sudo sed -i "7c Icon=/home/pi/$SCRIPTS_version/ICO_SOLOFUSION_ON.png" /home/pi/Abrir_solofusion.desktop
-sudo sed -i "11c Name[es_ES]=Cerrar solo FUSION" /home/pi/Abrir_solofusion.desktop
-sudo sed -i "12c SOLOFUSION=ON" /home/pi/status.ini
-cd /home/pi
-sudo cp Abrir_solofusion.desktop /home/pi/Desktop
 
-sudo rm /home/pi/Abrir_solofusion.desktop
+
+sed -i "6c Exec=sh -c 'cd /home/pi/BOOKWORM;sh cerrar_solofusion.sh'" /home/pi/Abrir_solofusion.desktop
+sed -i "7c Icon=/home/pi/BOOKWORM/ICO_SOLOFUSION_ON.png" /home/pi/Abrir_solofusion.desktop
+sed -i "11c Name[es_ES]=Cerrar solo FUSION" /home/pi/Abrir_solofusion.desktop
+sed -i "12c SOLOFUSION=ON" /home/pi/status.ini
+
+
+
+
 
 cd /home/pi/YSFClients/YSFGateway
 xterm -geometry 85x12+1215+296 -bg black -fg orange -fa ‘verdana’ -fs 9x -T CONSOLA_YSFGateway -e ./YSFGateway YSFGateway.ini & 
