d2e07d8e2a0728d510373b7f8d4e49227182e83e
Manel <ea3eiz@ea3eiz.com>
2024-01-04 18:16:36 +0100

    1

--- ejecutar_solofusion.sh
+++ ejecutar_solofusion.sh
@@ -27,7 +27,7 @@ sed -i "6c $frecuencia" /home/pi/INFO_RXF
 SCRIPTS_version=$(awk "NR==3" /home/pi/version-fecha-actualizacion)
 cd /home/pi/Desktop
 sudo cp Abrir_solofusion.desktop /home/pi
-sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh cerrar_solofusion.sh'" /home/pi/Abrir_solofusion.desktop
+sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sh cerrar_solofusion.sh'" /home/pi/Abrir_solofusion.desktop
 sed -i "7c Icon=/home/pi/$SCRIPTS_version/ICO_SOLOFUSION_ON.png" /home/pi/Abrir_solofusion.desktop
 sed -i "11c Name[es_ES]=Cerrar solo FUSION" /home/pi/Abrir_solofusion.desktop
 sed -i "12c SOLOFUSION=ON" /home/pi/status.ini
@@ -41,17 +41,17 @@ xterm -geometry 85x12+1215+296 -bg black -fg orange -fa ‘verdana’ -fs 9x -T 
 cd /home/pi/MMDVMHost
 xterm -geometry 85x12+612+296 -bg black -fg orange -fa ‘verdana’ -fs 9x -T CONSOLA_SOLOFUSION -e sudo ./MMDVMFUSION MMDVMFUSION.ini
 
-cd /home/pi/Desktop
-sudo cp Abrir_solofusion.desktop /home/pi
-
-sed -i "6c Exec=sh -c 'cd /home/pi/BOOKWORM/;sudo sh ejecutar_solofusion.sh'" /home/pi/Abrir_solofusion.desktop
-sed -i "7c Icon=/home/pi/$SCRIPTS_version/ICO_SOLOFUSION_OFF.png" /home/pi/Abrir_solofusion.desktop
-sed -i "11c Name[es_ES]=Abrir solo FUSION" /home/pi/Abrir_solofusion.desktop
-sed -i "12c SOLOFUSION=OFF" /home/pi/status.ini
-cd /home/pi
-sudo cp Abrir_solofusion.desktop /home/pi/Desktop
-
-sudo rm /home/pi/Abrir_solofusion.desktop
-
-sudo killall -9 MMDVMFUSION
+#cd /home/pi/Desktop
+#sudo cp Abrir_solofusion.desktop /home/pi
+#
+#sed -i "6c Exec=sh -c 'cd /home/pi/BOOKWORM/;sudo sh ejecutar_solofusion.sh'" /home/pi/Abrir_solofusion.desktop
+#sed -i "7c Icon=/home/pi/$SCRIPTS_version/ICO_SOLOFUSION_OFF.png" /home/pi/Abrir_solofusion.desktop
+#sed -i "11c Name[es_ES]=Abrir solo FUSION" /home/pi/Abrir_solofusion.desktop
+#sed -i "12c SOLOFUSION=OFF" /home/pi/status.ini
+#cd /home/pi
+#sudo cp Abrir_solofusion.desktop /home/pi/Desktop
+#
+#sudo rm /home/pi/Abrir_solofusion.desktop
+#
+#sudo killall -9 MMDVMFUSION
 
