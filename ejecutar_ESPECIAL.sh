5ad8792ad132904cb9556af3185ac53f751e232f
Manel <ea3eiz@ea3eiz.com>
2024-01-05 22:28:37 +0100

    1

--- ejecutar_ESPECIAL.sh
+++ ejecutar_ESPECIAL.sh
@@ -30,7 +30,7 @@ cd /home/pi/Desktop
 sudo cp Abrir_ESPECIAL.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_ESPECIAL.sh'" /home/pi/Abrir_ESPECIAL.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_ESPECIAL_ON.png" /home/pi/Abrir_ESPECIAL.desktop
-sed -i "10c Name[es_ES]=Cerrar Especial" /home/pi/Abrir_ESPECIAL.desktop
+#sed -i "10c Name[es_ES]=Cerrar Especial" /home/pi/Abrir_ESPECIAL.desktop
 sed -i "10c MMDVMESPECIAL=ON" /home/pi/status.ini
 cd /home/pi
 sudo cp Abrir_ESPECIAL.desktop /home/pi/Desktop
@@ -42,9 +42,9 @@ xterm -geometry 85x12+11+296 -bg black -fg yellow -fa ‘verdana’ -fs 9x -T CO
 
 cd /home/pi/Desktop
 sudo cp Abrir_ESPECIAL.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sh ejecutar_ESPECIAL.sh'" /home/pi/Abrir_ESPECIAL.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_ESPECIAL.sh'" /home/pi/Abrir_ESPECIAL.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_ESPECIAL_OFF.png" /home/pi/Abrir_ESPECIAL.desktop
-sed -i "10c Name[es_ES]=Abrir Especial" /home/pi/Abrir_ESPECIAL.desktop
+#sed -i "10c Name[es_ES]=Abrir Especial" /home/pi/Abrir_ESPECIAL.desktop
 sed -i "10c MMDVMESPECIAL=OFF" /home/pi/status.ini
 cd /home/pi
 sudo cp Abrir_ESPECIAL.desktop /home/pi/Desktop
