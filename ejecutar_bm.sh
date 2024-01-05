ae7ad98b324ea9df22c5d75dbce9a5ea7eada373
Manel <ea3eiz@ea3eiz.com>
2024-01-05 19:07:21 +0100

    Update ejecutar_bm.sh

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -2,17 +2,17 @@
 
 sudo killall -9 qt_editor_bm
 
-mode=`grep -n -m 1 "^UARTPort=" /home/pi/MMDVMHost/MMDVMBM.ini`
-buscar=":"
-caracteres=`expr index $mode $buscar`
-caracteres_linea=`expr $caracteres - 1`
-numero_linea_port=`expr substr $mode 1 $caracteres_linea`
-mode=$(awk "NR==$numero_linea_port" /home/pi/MMDVMHost/MMDVMBM.ini)
-puerto=`expr substr $mode 15 14`
-puerto="  "$puerto
-cd /home/pi/Desktop
-sudo cp RXF_BM.desktop /home/pi
-
+#mode=`grep -n -m 1 "^UARTPort=" /home/pi/MMDVMHost/MMDVMBM.ini`
+#buscar=":"
+#caracteres=`expr index $mode $buscar`
+#caracteres_linea=`expr $caracteres - 1`
+#numero_linea_port=`expr substr $mode 1 $caracteres_linea`
+#mode=$(awk "NR==$numero_linea_port" /home/pi/MMDVMHost/MMDVMBM.ini)
+#puerto=`expr substr $mode 15 14`
+#puerto="  "$puerto
+#cd /home/pi/Desktop
+#sudo cp RXF_BM.desktop /home/pi
+#
 frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/MMDVMBM.ini)
 frecuencia=`expr substr $frecuencia 13 17`
 frecuencia=$frecuencia$puerto
@@ -40,13 +40,14 @@ sudo rm /home/pi/Abrir_MMDVMBM.desktop
 cd /home/pi/MMDVMHost
 xterm -geometry 85x13+12+846 -bg brown -fg white -fa ‘verdana’ -fs 9x -T CONSOLA_BRANDMEISTER -e sudo ./MMDVMBM MMDVMBM.ini
 
-cd /home/pi/Desktop
-sudo cp Abrir_MMDVMBM.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sh ejecutar_bm.sh'" /home/pi/Abrir_MMDVMBM.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_BM_OFF.png" /home/pi/Abrir_MMDVMBM.desktop
-sed -i "10c Name[es_ES]=ON/OFF" /home/pi/Abrir_MMDVMBM.desktop
-sed -i "7c MMDVMBM=OFF" /home/pi/status.ini
-cd /home/pi
-sudo cp Abrir_MMDVMBM.desktop /home/pi/Desktop
-sleep 1
-sudo rm /home/pi/Abrir_MMDVMBM.desktop
+#cd /home/pi/Desktop
+#sudo cp Abrir_MMDVMBM.desktop /home/pi
+#sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sh ejecutar_bm.sh'" /home/pi/Abrir_MMDVMBM.desktop
+#sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_BM_OFF.png" /home/pi/Abrir_MMDVMBM.desktop
+#sed -i "10c Name[es_ES]=ON/OFF" /home/pi/Abrir_MMDVMBM.desktop
+#sed -i "7c MMDVMBM=OFF" /home/pi/status.ini
+#cd /home/pi
+#sudo cp Abrir_MMDVMBM.desktop /home/pi/Desktop
+#sleep 1
+#sudo rm /home/pi/Abrir_MMDVMBM.desktop
+
