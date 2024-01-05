56e318132f2f047803f498d2d41d3cb21a7b2f98
Manel <ea3eiz@ea3eiz.com>
2024-01-05 19:26:14 +0100

    1

--- cerrar_bm.sh
+++ cerrar_bm.sh
@@ -6,7 +6,7 @@ sudo cp Abrir_MMDVMBM.desktop /home/pi
 SCRIPTS_version=$(awk "NR==3" /home/pi/version-fecha-actualizacion)
 sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_bm.sh'" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_BM_OFF.png" /home/pi/Abrir_MMDVMBM.desktop
-sed -i "10c Name[es_ES]=ON/OFF" /home/pi/Abrir_MMDVMBM.desktop
+sed -i "10c Name[es_ES]=ON/OFF_abrir" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "7c MMDVMBM=OFF" /home/pi/status.ini
 
 cd /home/pi
