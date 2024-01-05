7391cadbfce4cb71abcfba7eec1f8d2235fe0bf4
Manel <ea3eiz@ea3eiz.com>
2024-01-05 22:13:43 +0100

    1

--- ejecutar_radio.sh
+++ ejecutar_radio.sh
@@ -42,7 +42,7 @@ xterm -geometry 85x13+1215+846 -bg blue -fg white -fa ‘verdana’ -fs 9x -T CO
 
 cd /home/pi/Desktop
 sudo cp Abrir_Radio.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sh sudo ejecutar_radio.sh'" /home/pi/Abrir_Radio.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh ejecutar_radio.sh'" /home/pi/Abrir_Radio.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_RADIO_OFF.png" /home/pi/Abrir_Radio.desktop
 #sed -i "10c Name[es_ES]=Abrir DMO" /home/pi/Abrir_Radio.desktop
 sed -i "5c MMDVM=OFF" /home/pi/status.ini
