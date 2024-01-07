778ad9ae0f8f69ccedffc88960a74c88cc8733dd
Manel <ea3eiz@ea3eiz.com>
2024-01-07 01:26:51 +0100

    1

--- ejecutar_dmrgateway.sh
+++ ejecutar_dmrgateway.sh
@@ -37,7 +37,7 @@ cp Abrir_dmrgateway.desktop /home/pi/
 #sleep 1
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_dmrgateway.sh'" /home/pi/Abrir_dmrgateway.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_DMRGATEWAY_ON.png" /home/pi/Abrir_dmrgateway.desktop
-sed -i "10c Name[es_ES]=Cerrar DMRGateway" /home/pi/Abrir_dmrgateway.desktop
+#sed -i "10c Name[es_ES]=Cerrar DMRGateway" /home/pi/Abrir_dmrgateway.desktop
 sed -i "19c DMRGateway=ON" /home/pi/status.ini
 #sleep 1
 cd /home/pi/
@@ -72,7 +72,7 @@ sudo cp Abrir_dmrgateway.desktop /home/pi/
 sleep 1
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_dmrgateway.sh'" /home/pi/Abrir_dmrgateway.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_DMRGATEWAY_OFF.png" /home/pi/Abrir_dmrgateway.desktop
-sed -i "10c Name[es_ES]=Abrir DMRGateway" /home/pi/Abrir_dmrgateway.desktop
+#sed -i "10c Name[es_ES]=Abrir DMRGateway" /home/pi/Abrir_dmrgateway.desktop
 sed -i "19c DMRGateway=OFF" /home/pi/status.ini
 sleep 1
 cd /home/pi/
