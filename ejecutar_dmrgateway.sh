525bc5dfc9b50495868dcf1b50320a36b98f0712
Manel <ea3eiz@ea3eiz.com>
2024-01-08 20:27:46 +0100

    Update ejecutar_dmrgateway.sh

--- ejecutar_dmrgateway.sh
+++ ejecutar_dmrgateway.sh
@@ -2,13 +2,13 @@
 
 #sudo systemctl stop mmdvm_bridge.service #esto se hace para que funcione el DMRGateway 
 
-mode=`grep -n -m 1 "^Port=" /home/pi/MMDVMHost/MMDVMDMRGateway.ini`
+mode=`grep -n -m 1 "^UARTPort=" /home/pi/MMDVMHost/MMDVMDMRGateway.ini`
 buscar=":"
 caracteres=`expr index $mode $buscar`
 caracteres_linea=`expr $caracteres - 1`
 numero_linea_port=`expr substr $mode 1 $caracteres_linea`
 mode=$(awk "NR==$numero_linea_port" /home/pi/MMDVMHost/MMDVMDMRGateway.ini)
-puerto=`expr substr $mode 11 9`
+puerto=`expr substr $mode 14 9`
 puerto="  "$puerto
 cd /home/pi/Desktop
 cp RXF_DMRGATEWAY.desktop /home/pi
