99621c4f6c56de380bfc93fb5ad355f49802487b
Manel <ea3eiz@ea3eiz.com>
2024-01-08 17:29:14 +0100

    1

--- cerrar_dv4mini.sh
+++ cerrar_dv4mini.sh
@@ -8,7 +8,7 @@ sudo cp Abrir_dv4mini.desktop /home/pi
 sleep 1
 sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_dv4mini.sh'" /home/pi/Abrir_dv4mini.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_DV4MINI_OFF.png" /home/pi/Abrir_dv4mini.desktop
-sed -i "10c Name[es_ES]=Abrir DV4mini" /home/pi/Abrir_dv4mini.desktop
+#sed -i "10c Name[es_ES]=Abrir DV4mini" /home/pi/Abrir_dv4mini.desktop
 sed -i "4c DV4mini=OFF" /home/pi/status.ini
 sleep 1
 cd /home/pi
