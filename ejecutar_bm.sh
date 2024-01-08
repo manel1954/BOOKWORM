1151ee84594a02483fbc2eea52e3962ad5ed6eae
Manel <ea3eiz@ea3eiz.com>
2024-01-08 21:53:28 +0100

    Update ejecutar_bm.sh

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -17,10 +17,10 @@ frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/MMDVMBM.ini)
 frecuencia=`expr substr $frecuencia 13 17`
 frecuencia=$frecuencia$puerto
 sed -i "12c Name=$frecuencia" /home/pi/RXF_BM.desktop
-sudo chmod 777 /home/pi/RXF_BM.desktop
-sudo mv /home/pi/RXF_BM.desktop /home/pi/Desktop
 
-#sudo rm /home/pi/RXF_BM.desktop
+sudo cp /home/pi/RXF_BM.desktop /home/pi/Desktop
+
+sudo rm /home/pi/RXF_BM.desktop
 
 #Escribe en el fichero INFO_RXF para poner los datos en el icono INFO TXF  
 sed -i "1c $frecuencia" /home/pi/INFO_RXF
