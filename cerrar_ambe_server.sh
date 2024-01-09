924f376a1cc6dbec60107c8cb24571906d11842c
Manel <ea3eiz@ea3eiz.com>
2024-01-09 11:25:40 +0100

    Update cerrar_ambe_server.sh

--- cerrar_ambe_server.sh
+++ cerrar_ambe_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo killall -9 AMBEserver
 SCRIPTS_version=$(awk "NR==3" /home/pi/version-fecha-actualizacion)
 cd /home/pi/Desktop
 sudo cp Abrir_ambe_server.desktop /home/pi
@@ -7,13 +8,13 @@ sed -i "4c Exec=sh -c 'cd /home/pi/BOOKWORM; sh ejecutar_ambe_server.sh'" /home/
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_AMBE_OFF.png" /home/pi/Abrir_ambe_server.desktop
 # sed -i "10c Name[es_ES]=Abrir AMBE SERVER" /home/pi/Abrir_ambe_server.desktop
 sed -i "11c AMBE_SERVER=OFF" /home/pi/status.ini
-sleep 1
+
 cd /home/pi
 sudo cp Abrir_ambe_server.desktop /home/pi/Desktop
-sleep 1
+
 sudo rm /home/pi/Abrir_ambe_server.desktop
 
-sudo killall -9 AMBEserver
+
 
 				
 						
