92c1e7a7a268f1a1ebab3786650c6af7d26f38c8
Manel <ea3eiz@ea3eiz.com>
2024-01-10 17:23:03 +0100

    Update actualizar_MMDVMHost.sh

--- actualizar_MMDVMHost.sh
+++ actualizar_MMDVMHost.sh
@@ -1,16 +1,16 @@
 ﻿#!/bin/bash
+                    #Copia el MMDVMHost Actual en .local
                     cd /home/pi
+                    sudo rm -R /home/pi/.local/MMDVMHost
                     sudo cp -R MMDVMHost /home/pi/.local/ 
+                    sudo chmod 777 -R /home/pi/.local/MMDVMHost
+                    # ------------------------------------------------
+                    
+                    #Actualiza el MMDVMHost
                     sudo rm -r /home/pi/MMDVMHost
                     sudo apt-get install build-essential git-core libi2c-dev i2c-tools lm-sensors
                     git clone https://github.com/g4klx/MMDVMHost
-                    
-                    #cd MMDVMHost
-                    #git clone https://github.com/hallard/ArduiPi_OLED
-                    #cd ArduiPi_OLED
-                    #sudo make clean
-                    #sudo make
-                                    
+                                 
                     cd /home/pi/MMDVMHost
 
                     HOY=$(date +%Y%m%d)
@@ -22,15 +22,9 @@
                     sed -i "22c $FIJA$HOY1$PUNTO" /home/pi/MMDVMHost/Version.h
 
                     make
-                    sudo make install
-                    #make clean
-                    #make -f Makefile.Pi.OLED
-
-                    #Instala la secion [NextionDriver] en todos los .ini y todas sus memorias
-                    cd /home/pi/NextionDriverInstaller
-                    sudo ./NextionDriver_ConvertConfig /home/pi/MMDVMHost/MMDVM.ini
-                    sleep 2                  
+                    sudo make install           
                     
+                    #Cambia estos parametros en el MMDVM.ini
                     sed -i "5c Duplex=0" /home/pi/MMDVMHost/MMDVM.ini
                     sed -i "52c # UARTSpeed=460800" /home/pi/MMDVMHost/MMDVM.ini
                     sed -i "229c Type=Direct" /home/pi/MMDVMHost/MMDVM.ini
@@ -48,10 +42,11 @@
                     cp MMDVMHost MMDVMDMR2M17
                     cp MMDVMHost MMDVMNXDN
                     cp MMDVMHost MMDVMESPECIAL
-                    cp MMDVMHost MMDVMDMRGATEWAY                 
+                    cp MMDVMHost MMDVMDMRGATEWAY                                     
                     
-                    # Crea el fichero para que arranque NextionDriver ==================
-                    cp MMDVM.ini MMDVM.ini_NextionDriver
+                    # Hace que el fichero TODOS_LOS_INIS sea igual al de MMDVM.ini para que luego 
+                    # el Editor General lo utilice
+                    cp MMDVM.ini TODOS_LOS_INIS.ini
 
                     # Crea los ficheros .ini y Memorias ==================
                     cp MMDVM.ini MMDVM.ini_original
@@ -85,17 +80,11 @@
                     cp MMDVM.ini MMDVMESPECIAL.ini_copia3                
 
                     cp MMDVM.ini MMDVMNXDN.ini
-                    
-                    cp MMDVM.ini TODOS_LOS_INIS.ini
-
-                    cp MMDVM.ini MMDVMDMR2YSF.ini
-                    
-                    cp MMDVM.ini MMDVMDMR2NXDN.ini
-
-                    cp MMDVM.ini MMDVMDMR2M17.ini
-                  
+                    cp MMDVM.ini MMDVMDMR2YSF.ini                    
+                    cp MMDVM.ini MMDVMDMR2NXDN.ini                  
                     cp MMDVM.ini MMDVMDMRGateway.ini 
                     
+                    # Hace cambios en los ficheros siguientes para el buen funcionamiento de ellos
                     sed -i "52c # UARTSpeed=460800" /home/pi/MMDVMHost/MMDVMDMRGateway.ini
                     sed -i "229c Type=Gateway" /home/pi/MMDVMHost/MMDVMDMRGateway.ini
                     sed -i "231c LocalPort=62032" /home/pi/MMDVMHost/MMDVMDMRGateway.ini
@@ -103,7 +92,7 @@
                     sed -i "52c # UARTSpeed=460800" /home/pi/MMDVMHost/MMDVMDMR2YSF.ini
                     sed -i "231c LocalPort=62032" /home/pi/MMDVMHost/MMDVMDMR2YSF.ini
                     sed -i "231c LocalPort=62032" /home/pi/MMDVMHost/MMDVMDMR2NXDN.ini
-                    sed -i "231c LocalPort=62037" /home/pi/MMDVMHost/MMDVMDMR2M17.ini
+                    # -----------------------------------------------------------------------------
 
                     sudo chmod 777 -R /home/pi/MMDVMHost
                 
