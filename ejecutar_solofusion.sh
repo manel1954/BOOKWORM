063c84a5d1c0df2171bcf5a7374769bf649000db
Manel <ea3eiz@ea3eiz.com>
2024-01-13 17:01:26 +0100

    Update ejecutar_solofusion.sh

--- ejecutar_solofusion.sh
+++ ejecutar_solofusion.sh
@@ -41,17 +41,17 @@ xterm -geometry 85x12+1215+296 -bg black -fg orange -fa ‘verdana’ -fs 9x -T 
 cd /home/pi/MMDVMHost
 xterm -geometry 85x12+612+296 -bg black -fg orange -fa ‘verdana’ -fs 9x -T CONSOLA_MMDVMFUSION -e sudo ./MMDVMFUSION MMDVMFUSION.ini
 
-cd /home/pi/Desktop
-sudo cp Abrir_solofusion.desktop /home/pi
-
-sed -i "6c Exec=sh -c 'cd /home/pi/BOOKWORM/;sudo sh ejecutar_solofusion.sh'" /home/pi/Abrir_solofusion.desktop
-sed -i "7c Icon=/home/pi/$SCRIPTS_version/ICO_SOLOFUSION_OFF.png" /home/pi/Abrir_solofusion.desktop
-#sed -i "11c Name[es_ES]=Abrir solo FUSION" /home/pi/Abrir_solofusion.desktop
-sed -i "12c SOLOFUSION=OFF" /home/pi/status.ini
-cd /home/pi
-sudo cp Abrir_solofusion.desktop /home/pi/Desktop
-
-sudo rm /home/pi/Abrir_solofusion.desktop
-
-sudo killall -9 MMDVMFUSION
+#cd /home/pi/Desktop
+#sudo cp Abrir_solofusion.desktop /home/pi
+#
+#sed -i "6c Exec=sh -c 'cd /home/pi/BOOKWORM/;sudo sh ejecutar_solofusion.sh'" /home/pi/#Abrir_solofusion.desktop
+#sed -i "7c Icon=/home/pi/$SCRIPTS_version/ICO_SOLOFUSION_OFF.png" /home/pi/Abrir_solofusion.#desktop
+##sed -i "11c Name[es_ES]=Abrir solo FUSION" /home/pi/Abrir_solofusion.desktop
+#sed -i "12c SOLOFUSION=OFF" /home/pi/status.ini
+#cd /home/pi
+#sudo cp Abrir_solofusion.desktop /home/pi/Desktop
+#
+#sudo rm /home/pi/Abrir_solofusion.desktop
+#
+#sudo killall -9 MMDVMFUSION
 
